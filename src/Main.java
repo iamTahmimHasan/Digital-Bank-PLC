81ca8b05093c93c6bf5bf6c7518cae792d44402e
Tahmim Hasan Sany <sanytahmim@gmail.com>
2023-11-26 13:08:21 +0600

    JFrame init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Program Start working...!");
+        new Login();
     }
 }
---
+++ src/login.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class Login extends JFrame {
+    Login(){
+        setSize(800, 480);
+        setVisible(true);
+
+    }
+}

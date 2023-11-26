8f5c9c587bd65c6103e949888a123350e820d53b
Tahmim Hasan Sany <sanytahmim@gmail.com>
2023-11-26 15:32:02 +0600

    icon added

--- src/login.java
+++ src/login.java
@@ -1,8 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.logging.Level;
+
 public class Login extends JFrame {
     Login(){
         setSize(800, 480);
         setVisible(true);
+        setLocation(350,200);
+        setTitle("Digital Bank PLC");
+        ImageIcon i1 =new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
+        ImageIcon logo = new ImageIcon(i2);
+        JLabel iconLabel = new JLabel(logo);
+        iconLabel.setBounds(70,10,100,100);
+        add(iconLabel);
 
     }
 }

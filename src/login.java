053a4aeeddd086b4deb484159ba9267f1494b6de
Tahmim Hasan Sany <sanytahmim@gmail.com>
2023-11-26 19:38:46 +0600

    Day Push Commit

--- src/login.java
+++ src/login.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.logging.Level;
+
 
 public class Login extends JFrame {
     Login(){
@@ -8,12 +8,27 @@ public class Login extends JFrame {
         setVisible(true);
         setLocation(350,200);
         setTitle("Digital Bank PLC");
-        ImageIcon i1 =new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        setLayout(null);
+
+        ImageIcon appIcon =new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        setIconImage(appIcon.getImage());
+
+        ImageIcon i1 =new ImageIcon(ClassLoader.getSystemResource("icons/logobr.png"));
         Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
         ImageIcon logo = new ImageIcon(i2);
         JLabel iconLabel = new JLabel(logo);
         iconLabel.setBounds(70,10,100,100);
         add(iconLabel);
+
+
+        getContentPane().setBackground(new Color(0x00b894));
+        JLabel title = new JLabel("Welcome to Digital Bank Network");
+        title.setBounds(200,40,500,40);
+        title.setFont(new Font("Oswald",Font.BOLD,30));
+        add(title);
+
+
+
 
     }
 }

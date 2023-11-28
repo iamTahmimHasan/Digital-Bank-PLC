3749a6689843bd80dd892e2f6e183946b25ff579
Tahmim Hasan Sany <sanytahmim@gmail.com>
2023-11-28 11:26:21 +0600

    Fixed swing component indent

--- src/login.java
+++ src/login.java
@@ -2,25 +2,29 @@ import javax.swing.*;
 import java.awt.*;
 
 
+
 public class Login extends JFrame {
     Login(){
+        //Screen Configuration
         setSize(800, 480);
         setVisible(true);
         setLocation(350,200);
         setTitle("Digital Bank PLC");
         setLayout(null);
 
+        //APP Icon Configuration
         ImageIcon appIcon =new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
         setIconImage(appIcon.getImage());
 
-        ImageIcon i1 =new ImageIcon(ClassLoader.getSystemResource("icons/logobr.png"));
+        // Logo Configuration
+        ImageIcon i1 =new ImageIcon(ClassLoader.getSystemResource("icons/logoWithoutBackground.png"));
         Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
         ImageIcon logo = new ImageIcon(i2);
         JLabel iconLabel = new JLabel(logo);
         iconLabel.setBounds(70,10,100,100);
         add(iconLabel);
 
-
+        // Screen Title Configuration
         getContentPane().setBackground(new Color(0x00b894));
         JLabel title = new JLabel("Welcome to Digital Bank Network");
         title.setBounds(200,40,500,40);
